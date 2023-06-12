eb645015eb23fd730b5c43492b99e753863584d5
pavelminsk1979 <pavelminsk1979@mail.ru>
2023-06-12 23:33:54 +0300

    router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
+import {Navigate, NavLink, Route, Routes} from 'react-router-dom'
+import {Registation} from "Registation";
+import {Login} from "Login";
+
 
 function App() {
-  return (
-    <div >
-    #3########
-    </div>
-  );
+    return (
+        <div>
+
+            <NavLink to={'/login'}>Login--</NavLink>
+            <NavLink to={'/register'}>Registation--</NavLink>
+
+
+            <Routes>
+                <Route path="/register" element={<Registation/>}/>
+                <Route path="login" element={<Login/>}/>
+
+                <Route path="/" element={<Navigate to="/login"/>}/>
+                <Route path="*" element={<Navigate to="/404"/>}/>
+                <Route path="/404" element={<h2>404: СТРАНИЦА НЕ НАЙДЕНА...ОШИБКА!</h2>}/>
+            </Routes>
+
+        </div>
+
+    );
 }
 
 export default App;
---
+++ src/Registation.tsx
@@ -0,0 +1,5 @@
+export const Registation = () => {
+  return(
+      <div>registration</div>
+  )
+}
